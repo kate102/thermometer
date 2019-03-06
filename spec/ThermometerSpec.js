ea3c114cfdd182bba91a69c0bf3c83ced8bfe6be
Kate102 <student@Admins-MacBook-Pro-4.local>
2019-03-06 15:07:39 +0000

    Added minimum temp check

--- spec/ThermometerSpec.js
+++ spec/ThermometerSpec.js
@@ -12,12 +12,27 @@ describe("Thermometer", function() {
   it('increases temperature by 1', function(){
     thermometer.increaseTemperature();
     expect(thermometer.currentTemp).toEqual(21);
-
   })
 
   it('decrease temperature by 1', function(){
     thermometer.decreaseTemperature();
     expect(thermometer.currentTemp).toEqual(19);
-
   })
+
+  it("the minimum temperature is 10 degrees", function() {
+    expect(thermometer.minimumTemp).toEqual(10);
+  });
+
+  it("the minimum temperature cant be exceeded", function() {
+    count = 1
+    while (count < 11){
+      thermometer.decreaseTemperature();
+      count++;
+    }
+    expect(thermometer.currentTemp).toEqual(10);
+    expect(function() {
+      thermometer.decreaseTemperature();
+    }).toThrowError("the minimum temperature cant be exceeded");
+  });
+
 });
--- src/Thermometer.js
+++ src/Thermometer.js
@@ -1,10 +1,17 @@
 function Thermometer() {
   this.currentTemp = 20
+  this.minimumTemp = 10
 }
 
 Thermometer.prototype.increaseTemperature = function() {
   this.currentTemp++;
 }
 Thermometer.prototype.decreaseTemperature = function() {
-  this.currentTemp--;
+
+  if (this.currentTemp > 10){
+      this.currentTemp--;
+  }
+  else {
+    throw new Error("the minimum temperature cant be exceeded");
+  }
 }
